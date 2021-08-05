30def06d8bd3ce088e8f9d5a1e8233361ee14b8b
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-08-06 00:33:36 +0300

    fixing migrations

--- console/migrations/m210805_194734_user_info_table.php
+++ console/migrations/m210805_194734_user_info_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Class m210804_194734_user_info_table
  */
-class m210804_194734_user_info_table extends Migration
+class m210805_194734_user_info_table extends Migration
 {
 
     public function up()
---
+++ console/migrations/m210805_212510_add_post_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210805_212510_add_post_table
+ */
+class m210805_212510_add_post_table extends Migration
+{
+
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->createTable('user_info', [
+            'id' => $this->primaryKey(),
+            'photo' => $this->string(),
+            'description' => $this->string(),
+            'tagId'=>$this->integer()
+        ]);
+    }
+
+    public function down()
+    {
+        echo "m210805_212510_add_post_table cannot be reverted.\n";
+
+        return false;
+    }
+
+}
